e0325044b9b11697768983cf3f54d601919804a0
Gourav <gouravmahesh8@gmail.com>
2025-06-22 12:07:30 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
-// === frontend/src/index.js ===
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
-
-// === frontend/src/App.js ===
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PlayerList from "./PlayerList";
-import PlayerDetail from "./PlayerDetail";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PlayerList />} />
-        <Route path="/player/:name" element={<PlayerDetail />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
